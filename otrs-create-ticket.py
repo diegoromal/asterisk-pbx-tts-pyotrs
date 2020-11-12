14669a412461cc0075c0e4b7393edfd3f7a4ec76
diegoromanio <diego@qosit.com.br>
2020-11-12 15:51:59 -0300

    final-project-push

--- otrs-create-ticket.py
+++ otrs-create-ticket.py
@@ -1,27 +1,48 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import sys
-reload(sys)
-sys.setdefaultencoding('utf-8')
 from pyotrs import Article, Client, DynamicField, Ticket
 
-title = sys.argv[1]
-body  = sys.argv[2]
+URL = "https://url-otrs"
+USER = "user"
+PASSWORD = "pasword"
+variable = '"OTRSTICKET"'
+aspas = '"'
+variableValue = aspas + ticketnumber + aspas
 
-client = Client("https://URL", "USER", "PASSWORD")
-client.session_create()
 
-new_ticket = Ticket.create_basic(
-                                Title=title,
+if(len(sys.argv) == 3):
+	title = sys.argv[1]
+	body = sys.argv[2]
+
+	client = Client(URL, USER, PASSWORD)
+	client.session_create()
+
+	new_ticket = Ticket.create_basic(
+				Title=title,
                                 Queue="Novos tickets via telefone",
                                 State=u"new",
                                 Priority=u"Media prioridade",
                                 CustomerUser="pbx@qosit.com.br"
-                                )
+				)
+				
+	first_article = Article({"Subject": title, "Body": body})
 
-first_article = Article({"Subject": title, "Body": body})
+	create_ticket = client.ticket_create(new_ticket, first_article)
 
-create_ticket = client.ticket_create(new_ticket, first_article)
+	ticketnumber = create_ticket['TicketNumber']
 
+	variable = '"OTRSTICKET"'
+	aspas = '"'
+	variableValue = aspas + ticketnumber + aspas
 
-print "SET VARIABLE \"RETURN\" \"$create_ticket\"\n";
+else:
+    title = " "
+    body  = " "
+    ticketnumber = " "
+
+if (ticketnumber == " " or (title == " " and body == " ")):
+   print("set variable \"RETURN\" \"Ticket não criado, entre em contato com nosso time de suporte!\"")
+else:
+   print("set variable \"RETURN\" \"Seu ticket é:\"")
+   print("set variable", variable, variableValue)
