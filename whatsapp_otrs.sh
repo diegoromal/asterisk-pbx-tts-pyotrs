c035f7c196332199df7c3039b19f3375e89aa28b
diegoromanio <diego@qosit.com.br>
2020-11-16 21:07:22 -0300

    Seg Nov 16 21:07:22 -03 2020

--- whatsapp_otrs.sh
+++ whatsapp_otrs.sh
@@ -2,6 +2,7 @@
 
 #coleta informaçoes zabbix
 data=$1
+ticket=$2
 
 #informações mandeumzap
 number="55${data}"
@@ -10,12 +11,14 @@ token="37c69527a972b78e6420518f2f6a412d245b8085"
 
 echo "${number}"
 
+
+
 curl --location --request POST 'https://qosit-api.digisac.app/v1/messages' \
 --header 'Authorization: Bearer '${token}'' \
 --header 'Content-Type: application/x-www-form-urlencoded' \
 --data-urlencode 'number=='${number}'' \
 --data-urlencode 'dontSendUserName=true' \
 --data-urlencode 'serviceId=6e1d7714-a8aa-438a-9ea5-28f41118a01b' \
---data-urlencode 'text=Você solicitou atendimento da Q.o.S pelo WhatsApp, em breve um atendente entrará em contato! *O horário de atendimento deste canal é de segunda à sexta-feira entre 08:30 e 17:30hrs pelo horário de Brasília, exceto em feriados nacionais.*'
+--data-urlencode 'text=Você acabou de abrir um ticket no suporte da Q.o.S! Aguarde, em breve nosso time entrará em contato! Para acessá-lo clique em: https://suporte.qosit.com.br/otrs/customer.pl?Action=CustomerTicketZoom;TicketNumber='${ticket}''
 
 exit 0
